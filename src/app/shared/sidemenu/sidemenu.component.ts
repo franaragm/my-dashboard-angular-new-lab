89df8f4f14ef7cc1950418c13105047a3061b99a
Fran Aragon <franaragonmesa@gmail.com>
2025-01-17 18:06:45 +0100

    update

--- src/app/shared/sidemenu/sidemenu.component.ts
+++ src/app/shared/sidemenu/sidemenu.component.ts
@@ -1,20 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, Signal } from '@angular/core';
 import { routes } from '../../app.routes';
 import { RouterModule } from '@angular/router';
+import { AuthStore } from '../../dashboard/pages/auth/auth.store';
 
 @Component({
-    selector: 'app-sidemenu',
-    imports: [RouterModule],
-    templateUrl: './sidemenu.component.html',
-    styleUrl: './sidemenu.component.scss'
+  selector: 'app-sidemenu',
+  imports: [RouterModule],
+  templateUrl: './sidemenu.component.html',
+  styleUrl: './sidemenu.component.scss'
 })
 export class SidemenuComponent {
+  private readonly authStore = inject(AuthStore);
 
   public menuItems = routes
     .map((route) => route.children ?? [])
     .flat()
     .filter((route) => route && route.path)
     .filter((route) => !route.path?.includes(':'));
+
+  /**
+   * Selects if the user is authenticated
+   * */
+  protected isAuthenticated: Signal<boolean> = this.authStore.selectIsAuthenticated;
+
+  /**
+   * Logs out the user
+   */
+  protected logout() {
+    this.authStore.dispatchLogout();
+  }
 
   constructor() {
     // const dashboardRoutes = routes
